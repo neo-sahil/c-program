afa176e617b059980f356b3194150c21d4b97fea
Sahil Sharma <16tec2cs022@vgu.ac.in>
2019-09-24 16:05:04 +0530

    test your c skills ch-5

---
+++ recursion1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void fun(int);
+void main()
+{
+    int a;
+    a=3;
+    fun(a);
+    printf("\n");
+}
+
+void fun(int n)
+{
+    if(n>0)
+    {
+        fun(--n);
+        printf("%d ",n);
+        fun(--n);
+    }
+}
